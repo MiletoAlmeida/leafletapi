7d648373daa9bf91e3caec6ac6d88cf0ebdefd03
Mileto Almeida <almeidamileto@gmail.com>
2025-04-24 01:13:32 -0300

    feat: Add cache configuration and enhance WebClient setup
    Introduced a caching mechanism using `ConcurrentMapCacheManager` to handle caches for medicines, leaflets, and searches. Enhanced the WebClient configuration with custom timeouts, memory limits, and logging for improved request handling and debugging.

--- src/main/java/com/miletoalmeida/leafletapi/config/WebClientConfig.java
+++ src/main/java/com/miletoalmeida/leafletapi/config/WebClientConfig.java
@@ -1,14 +1,47 @@
 package com.miletoalmeida.leafletapi.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import reactor.netty.http.client.HttpClient;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        reactor.netty.http.client.HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 30000)
+                .responseTimeout(Duration.ofSeconds(30))
+                .doOnConnected(conn -> conn
+                        .addHandlerLast(new ReadTimeoutHandler(30, TimeUnit.SECONDS))
+                        .addHandlerLast(new WriteTimeoutHandler(30, TimeUnit.SECONDS)));
+
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
+                .build();
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .exchangeStrategies(exchangeStrategies)
+                .filter(logRequest())
+                .build();
+    }
+
+    private ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            System.out.println("Request: " + clientRequest.method() + " " + clientRequest.url());
+            return Mono.just(clientRequest);
+        });
     }
 }
