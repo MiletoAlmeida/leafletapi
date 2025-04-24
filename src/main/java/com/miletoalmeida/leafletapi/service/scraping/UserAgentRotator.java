ca1492d054f5945725df7fc315633f88d25bd008
Mileto Almeida <almeidamileto@gmail.com>
2025-04-24 19:56:58 -0300

    refactor: Refactor UserAgentRotator package and update dependencies.
    Moved UserAgentRotator to the "scraping" package for better organization. Replaced `List.of` with `Arrays.asList` for compatibility and updated the user agents list to include a Linux entry.

--- src/main/java/com/miletoalmeida/leafletapi/service/scraping/UserAgentRotator.java
+++ src/main/java/com/miletoalmeida/leafletapi/service/scraping/UserAgentRotator.java
@@ -1,17 +1,17 @@
-package com.miletoalmeida.leafletapi.service;
+package com.miletoalmeida.leafletapi.service.scraping;
 
 import org.springframework.stereotype.Component;
-
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 @Component
 public class UserAgentRotator {
-    private final List<String> userAgents = List.of(
+    private final List<String> userAgents = Arrays.asList(
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15"
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
     );
     
     private final Random random = new Random();
